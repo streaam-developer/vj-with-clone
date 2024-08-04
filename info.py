79b475b53fb113849bbf4a66bbe8ba47b9d95b8e
streaam-developer <163761596+streaam-developer@users.noreply.github.com>
2024-08-04 16:24:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -112,7 +112,7 @@ SHORTLINK_API = environ.get('SHORTLINK_API', 'hRPS5vvZc0OGOEUQJMJzPiojoVK2')
 
 # Others
 MAX_B_TN = environ.get("MAX_B_TN", "5")
-PORT = environ.get("PORT", "5551")
+PORT = environ.get("PORT", "4896")
 MSG_ALRT = environ.get('MSG_ALRT', 'Hello My Dear Friends ❤️')
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
